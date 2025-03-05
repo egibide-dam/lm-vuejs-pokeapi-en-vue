ce82db158af6306861604584e5351bd868d052a2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-03-05 17:16:19 +0100

    Abrir el link en otra ventana

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,6 +3,6 @@
 
 <template>
     <p class="alert alert-warning">
-        Aplicación Vue 3 que consume el API REST de <a href="https://pokeapi.co/docs/v2">PokéAPI</a>.
+        Aplicación Vue 3 que consume el API REST de <a href="https://pokeapi.co/docs/v2" target="_blank">PokéAPI</a>.
     </p>
 </template>
