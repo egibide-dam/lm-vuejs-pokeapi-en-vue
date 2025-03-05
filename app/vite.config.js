a5cf87f22ca9ff7237b6d4ff56741c494d5a9454
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-03-05 17:02:12 +0100

    Ruta para GitHub Pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
-    base: ''    // Por defecto es /
+    base: '/lm-vuejs-pokeapi-en-vue/'    // Por defecto es /
 })
